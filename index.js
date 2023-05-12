0b72bf0431fbdb8c65703677e137739665a7e50b
Krish <nayakkrish11527@gmail.com>
2023-05-12 18:30:57 +0530

    connected db with prisma to project

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 8000;
+const userRouter = require("./routes/userRoutes");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "15.206.6.55",
+  user: "hourlyrooms",
+  password: "Hourlyr0Oms@1584",
+  // database: "node_crud",
+});
 
 app.use(express.static(path.join(__dirname, "static")));
-app.use("/", require(path.join(__dirname, "routes/hotel.js")));
-app.use("/", require(path.join(__dirname, "routes/user.js")));
+app.use("/", require(path.join(__dirname, "routes/hotelRoutes.js")));
+// app.use("/", require(path.join(__dirname, "routes/userRoutes.js")));
+app.use("/", userRouter);
 
 // app.get("/", (req, res) => {
 //   res.sendFile(path.join(__dirname, "home.html"));
