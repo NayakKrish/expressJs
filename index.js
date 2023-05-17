d1a4b04be701a332e70aee00922410835ceff2b4
Krish <nayakkrish11527@gmail.com>
2023-05-17 13:43:10 +0530

    added cors

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const port = 8000;
 const userRouter = require("./routes/userRoutes");
 const dotenv = require("dotenv");
 const notesRouter = require("./routes/notesRoute");
+const cors = require("cors");
 
 app.use(express.static(path.join(__dirname, "static")));
 app.use("/", require(path.join(__dirname, "routes/hotelRoutes.js")));
 app.use("/user", userRouter);
 app.use("/notes", notesRouter);
-// app.use(cors());
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`Hotel app listening on port http://localhost:${port}`);
